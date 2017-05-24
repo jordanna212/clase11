1c15911ab86ca6f343e504ce0155a961e95904af
jordanna212 <jddelahoz@uc.cl>
2017-05-24 13:29:01 -0400

    Subiendo Archivos

---
+++ about.php
@@ -0,0 +1,21 @@
+<?php include('header.php')?>
+
+<div class="row">
+<div class="col-sm-7">
+<h3>Sobre el proyecto</h3>
+
+<h5> Quiero crear mi tienda de panqueques y competir contra el Inser Coin.</h5>
+<p> En el futuro pretendo tener franquicias y dominar el mundo con panqueques.</p>
+<p>Desde que tengo memoria comencé a mezclarme con este mundo ficticio,
+  desde a pasar a teletubis. Cuando era pequeña veía Drabon Ball en "Zoolo TV"
+  mientras tomaba 	desayuno en casino de mi colegio, ya en la tarde cuando llegaba a mi casa
+  no podia resistirme a la idea de ver En el club de los tigritos, Digimon,
+  Sakura Card Captor, Pokemon, Detective Conan, Inuyasha, Samurai X, 	Escuela de Detectives. </p>
+  <p> Y cuando se trataba de ver una películas con amigos o junto a mi hermano 	siempre mirábamos
+  las sagas de Star Wars, Harry potter o  el Señor de los 	anillos.</p>
+
+
+</div><!--/col-sm-12-->
+</div><!--/row->
+
+<?php include('footer.php')?>
---
+++ footer.php
@@ -0,0 +1,10 @@
+</div><!--/container-->
+<footer class="footer">
+<div class="container">
+<p class="text-muted"><?php echo $title;?> por Jordanna De La Hoz. 24 de mayo, 2017. · Que sería de mi sin el azúcar ·.</p>
+</div>
+</footer>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
